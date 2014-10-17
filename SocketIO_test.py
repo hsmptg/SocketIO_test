526a4faf06ddb597a6322b4c745fb00b13cc09a2
Helio Mendonca <hsmptg@gmail.com>
2014-10-17 08:11:11 +0100

    serial write

--- SocketIO_test.py
+++ SocketIO_test.py
@@ -1,15 +1,24 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
+import serial
+
+ser = serial.Serial()
 
 print("Flask-SocketIO running...")
 app = Flask(__name__)
-app.debug = False
+app.debug = True
 socketio = SocketIO(app)
 
 @app.before_first_request
 def initialize():
-    print "Called only once, when the first request comes in"        
-        
+    print "Called only once, when the first request comes in"       
+    try:
+        ser.port = "/dev/ttyACM0"
+        ser.baudrate = 115200
+        ser.open()
+    except serial.SerialException as e:
+        print(e)
+                    
 @app.route('/')
 def index():
     print('Rendering index.html')
@@ -18,6 +27,10 @@ def index():
 @socketio.on('ledCtrl', namespace='/test')
 def ledCtrl(message):
     print(message['led'])
+    if message['led']:
+        ser.write('l1\r')
+    else:
+        ser.write('l0\r')
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=5001)
